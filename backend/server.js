b22a66c80a5984de880c624905054aad17033942
Quachbaohung1 <baohung9715@gmail.com>
2021-10-27 23:41:38 +0700

    final

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const app = require('./backend/app')
-const connectDatabase = require('./backend/config/database')
+const app = require('./app')
+const connectDatabase = require('./config/database')
 
 const dotenv = require('dotenv');
 
@@ -18,15 +18,9 @@ dotenv.config({ path: 'backend/config/config.env'})
 // Connecting to DB
 connectDatabase(); 
 
-const PORT = process.env.PORT || 5000;
-
-//const server = app.listen(PORT, () => {
-//    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
-//})
-
-
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+})
 
 // Handle Unhandle Promise rejections
 process.on('unhandledRejection', err => {
